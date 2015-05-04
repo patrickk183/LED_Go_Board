4c3c6762dfe5c1a9ac734ea6bb1bd9b1e2487aa5
Patrick Wade Jr. Knauth <pwk183@linux2.cs.tamu.edu>
2015-05-04 16:34:59 -0500

    added forking for file execution

--- reversi/led-matrix/Menu_main.cpp
+++ reversi/led-matrix/Menu_main.cpp
@@ -1,4 +1,7 @@
 
+#include <exception>  // terminate()
+#include <cstdlib>    // abort()
+
 
 #include "ShowReversiBoard.h"
 #include "include/led-matrix.h"
@@ -6,6 +9,10 @@
 #include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
 
 int main(int argc, char **argv) {
 	//Go interface section of startup
@@ -13,6 +20,9 @@ int main(int argc, char **argv) {
 	int chain = 1;
 	int parallel = 1;
 	bool do_luminance_correct = true;
+	
+	pid_t pid;
+	char *child_args[5];
 
 	if (getuid() != 0) {
 	//"Must run as root to be able to access /dev/mem
@@ -32,7 +42,7 @@ int main(int argc, char **argv) {
 	Canvas *canvas = matrix;
 
 	int players = 1;
-	int mode = 1;
+	int mode = 0;
 	int size = 1;
 	int difficulty = 1;
 
@@ -46,56 +56,74 @@ int main(int argc, char **argv) {
 
 	// Image generating demo is created. Now start the thread.
 	image_gen->Start();
-
-	while(mode < 4) {
-		char input = getchar();
-
-		//change settings
-		switch (mode) {
-			case 1:
-				if (input == 'w' && players == 1) {
-					players++;
+	char input = getchar();
+	//change settings
+	switch (pid = fork()) {
+		case -1: 
+			perror("fork");
+			break;
+			
+		case 0:
+			switch (mode) {
+				case 1:
+					if (input == 'w' && players == 1) {
+						players++;
+					}
+					else if (input == 's' && players == 2) {
+						players--;
+					}
+					else if (input == '\n') {
+						if (players == 1) { 
+							mode++;
+							child_args[1] = "1";
+						}
+						else { 
+						child_args[0] = "2"; 
+						mode+2;
+						}
+					}
+					break;
+				case 2:
+					if (input == 'w' && difficulty < 3) {
+						difficulty++;
+					}
+					else if (input == 's' && difficulty > 1) {
+						difficulty--;
+					}
+					else if (input == '\n') {
+						mode++;
+						child_args[2] = "3";
+					}
+					break;
+				case 3:
+					if (input == 'w' && size == 1) {
+						size++;
+					}
+					else if (input == 's' && size == 2) {
+						size--;
+					}
+					else if (input == '\n') {
+						child_args[0] = "./Othello_size8";
+						execv("./Othello_size8 ",  child_args);
+						puts("Uh oh! If this prints, execv() must have failed");
+						exit(EXIT_FAILURE);
+					break;
+					}
+				default:
+					std::cout << "menu failed in main.c" << std::endl;
+					break;
 				}
-				else if (input == 's' && players == 2) {
-					players--;
-				}
-				else if (input == '\n') {
-					if (players == 1) { mode = 2; }
-					else { mode = 3; }
-				}
-				break;
-			case 2:
-				if (input == 'w' && difficulty < 3) {
-					difficulty++;
-				}
-				else if (input == 's' && difficulty > 1) {
-					difficulty--;
-				}
-				else if (input == '\n') {
-					mode++;
-				}
-				break;
-			case 3:
-				if (input == 'w' && size == 1) {
-					size++;
-				}
-				else if (input == 's' && size == 2) {
-					size--;
-				}
-				else if (input == '\n') {
-					mode++;
-				}
-				break;
-			default:
-				std::cout << "menu failed in main.c" << std::endl;
-				break;
-		}
-
+		default:
+			puts("End of fork\n");
+			break;
+	}
 		canvas->Clear();
 		delete image_gen;
 		image_gen = new Menu(canvas, mode, players, difficulty, size);
 		image_gen->Start();
-	}
+
+	
+  
 	canvas->Clear();
 	delete image_gen;
 
@@ -105,4 +133,5 @@ int main(int argc, char **argv) {
 	// Stop image generating thread.
 	delete image_gen;
 	delete canvas;
+	return 0;
 }
