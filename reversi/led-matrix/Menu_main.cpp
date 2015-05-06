b4e2c6d00302318dc16726e99d0fd24a7eea90de
Robert Bagott <bago37@tamu.edu>
2015-05-06 01:20:43 -0500

    commit

--- reversi/led-matrix/Menu_main.cpp
+++ reversi/led-matrix/Menu_main.cpp
@@ -343,7 +343,7 @@ int reversi_main8(int player_count, int depth)
             char input;
             //while (input != 'w' || input != 's' || input != 'a' ||;input != 'd' || input != '\n') { input = getchar(); }
 
-            while (getchar()) {
+            while (input = getchar()) {
               getchar(); //eat newline
               if (input == 'w') {
                 if (curs.gety()-1 >= 0) {
