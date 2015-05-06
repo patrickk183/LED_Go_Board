b2efcdb0a47953c1369ef8479ebbbed27071d841
Robert Bagott <bago37@tamu.edu>
2015-05-06 06:23:59 -0500

    debug statement

--- reversi/led-matrix/Menu_main.cpp
+++ reversi/led-matrix/Menu_main.cpp
@@ -71,6 +71,8 @@ int main(int argc, char **argv) {
 	if (image_gen == NULL) { return -1; }
   image_gen->Start();
   while (isNotSelected(1) && isNotSelected(2)) {usleep(1000); }
+  //debug
+  getchar();
   usleep(50000);
   splash_done = true;
   canvas->Clear();
