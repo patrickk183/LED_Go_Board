f8506f0200b777989919862600538661856d61c3
Robert Bagott <bago37@tamu.edu>
2015-05-06 02:32:17 -0500

    logic changes?

--- reversi/led-matrix/Menu_main.cpp
+++ reversi/led-matrix/Menu_main.cpp
@@ -279,7 +279,7 @@ int reversi_main8(int player_count, int depth)
               }
               usleep(10000);
             }
-            if( curs.getx() >= 0 && curs.gety() >= 0 && curs.getx() < SIZE && curs.gety() < SIZE && moves[curs.getx()][curs.gety()]) {
+            if(/* curs.getx() >= 0 && curs.gety() >= 0 && curs.getx() < SIZE && curs.gety() < SIZE && */moves[curs.getx()][curs.gety()]) {
               make_move(board, curs.gety(), curs.getx(), 'O');
               std::cout << "move made" << std::endl;
               no_of_moves++;              /* Increment move count */
@@ -370,7 +370,7 @@ int reversi_main8(int player_count, int depth)
               }
               usleep(10000);
             }
-            if( curs.getx() >= 0 && curs.gety() >= 0 && curs.getx() < SIZE && curs.gety() < SIZE && moves[curs.getx()][curs.gety()]) {
+            if(/* curs.getx() >= 0 && curs.gety() >= 0 && curs.getx() < SIZE && curs.gety() < SIZE && */moves[curs.getx()][curs.gety()]) {
               make_move(board, curs.getx(), curs.gety(), '@');
               std::cout << "move made" << std::endl;
               no_of_moves++;              /* Increment move count */
