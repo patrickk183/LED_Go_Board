c65ddf7c748d75f88cab47805430f78e140bdeac
Robert Bagott <bago37@tamu.edu>
2015-05-08 11:13:01 -0500

    URGH!

--- reversi/Menu_main.cpp
+++ reversi/Menu_main.cpp
@@ -43,7 +43,7 @@ void testInput()
 		isRight(2) == true) {
 
 		//exec the program
-		string filename = "/home/pi/Documents/led-go/LED_Go_Board/reversi/Menu_main";
+		std::string filename = "/home/pi/Documents/led-go/LED_Go_Board/reversi/Menu_main";
 		char* args[2] = {filename.c_str(), NULL};
 		execv(filename.c_str(), args);
 		perror("execve");
